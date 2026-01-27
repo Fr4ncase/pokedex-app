f7f790d6e8d54e9968f1897381de55b80e8db06e
Fr4ncase <franco01extreme@gmail.com>
2026-01-27 01:26:55 -0500

    Modificaciones menores

--- src/components/PokemonDetail.tsx
+++ src/components/PokemonDetail.tsx
@@ -7,10 +7,39 @@ interface PokemonCardProps {
 }
 
 export const PokemonDetail = ({ pokemon }: PokemonCardProps) => {
+  const spriteUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
+
   return (
     <main className='w-full flex-1 overflow-y-auto custom-scrollbar px-4'>
       <div className='w-full px-4'>
-        <h1>{pokemon.name}</h1>
+        <h1 className='uppercase text-center text-[40px] text-pokedex-gray mt-5'>
+          {pokemon.name}
+        </h1>
+        <div className='flex justify-center'>
+          <span className='border rounded w-fit px-1'>Poison Pin Pokemon</span>
+        </div>
+        <div className='flex justify-center'>
+          <div className='w-88 h-75'>
+            <div></div>
+          </div>
+          <div className='flex justify-center'>
+            <img
+              src={spriteUrl}
+              alt={pokemon.name}
+              className='object-contain'
+            />
+          </div>
+          <div className='w-88 h-75'>
+            <div></div>
+          </div>
+        </div>
+        <div>
+          <div className='flex justify-center'>
+            <span className='text-2xl border rounded px-1'>
+              EVOLUTION CHAIN
+            </span>
+          </div>
+        </div>
       </div>
     </main>
   );
